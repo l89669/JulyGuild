4b7a39bf7f8efcdd2240a571f9da25469340d995
July_ss <july_ss_2016@outlook.com>
2020-01-12 15:23:27 +0800

    更新

--- src/main/java/com/github/julyss2019/mcsp/julyguild/guild/Permission.java
+++ src/main/java/com/github/julyss2019/mcsp/julyguild/guild/Permission.java
@@ -3,11 +3,12 @@ package com.github.julyss2019.mcsp.julyguild.guild;
 import com.github.julyss2019.mcsp.julyguild.JulyGuild;
 
 public enum Permission {
-    GUILD_UPGRADE,
     MEMBER_KICK, // 成员踢出
-    MEMBER_SET_ADMIN, // 成员设置管理员
+    SET_SPAWN, // 设置主城
+    SET_MEMBER_PVP, // 成员PVP
     PLAYER_JOIN_CHECK, // 玩家审批
-    MEMBER_SET_PERMISSION // 成员设置权限
+    GUILD_UPGRADE,  // 公会升级
+    MANAGE_PERMISSION // 给予权限
     ;
 
     String getChineseName(Permission permission) {
